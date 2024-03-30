f1da21cdef786511792ac8f77195e25104285ac5
Chahid Hadifi <chahidhadifi@gmail.com>
2024-03-30 17:03:54 +0000

    add server.js file

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log("server running on port " + PORT);
+})
